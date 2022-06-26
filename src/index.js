79fdca30c6855cbb32481ced3d0f78b57bff8570
nayvcake <43297329+nayvcake@users.noreply.github.com>
2022-06-25 22:27:06 -0300

    fix(terminal): changing the effects in the code to avoid a spike in the cpu.
    Co-Authored-By: Nayv <43297329+nayvcake@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const fs = require('fs')
 const path = require('path')
-const {
-  spawn
-} = require('child_process')
 const EventEmitter = require('events')
+
+
 const openDir = async (dir) => {
   let files = []
   await fs.readdirSync(dir).map((a, b, c) => {
@@ -12,32 +11,88 @@ const openDir = async (dir) => {
   return files
 }
 
+
 const openFile = async (fileDir) => {
   let files = null
-  await fs.readFileSync(dir).map((a) => {
+  await fs.readFileSync(fileDir).map((a) => {
     files = a
   })
   return files
 }
 
+const openFileAndRead = async (fileDir) => {
+  let files = null
+  files = await fs.readFileSync(fileDir)
+  return files
+}
+
 
 const verify = async (file) => {
-  const isJsonAndFile = false
+  let isJsonAndFile = false
   try {
     JSON.parse(await openFile(file))
     isJsonAndFile = true
   } catch (_) {}
+
   return isJsonAndFile
 }
 
 
-const openTerminal = async (file, out) => {
-  const a = []
-  const b = []
-  const terminal = spawn('tsc', [`-p ${path.resolve('file')}`])
-  return terminal
+const openTerminal = async (file, options, optionsProject) => {
+  const {
+    spawn,
+    spawnSync
+  } = require('child_process');
+  const args = []
+  if (typeof file != 'string') {
+    throw Error('')
+  } else {
+    args.push('-p', file)
+  }
+  if (typeof options.watchMode === 'boolean') {
+    if (options.watchMode) {
+      args.push('-w')
+    }
+  }
+  try {
+    if (options.watchMode) {
+
+      return new Promise(async resolve => {
+
+        const terminal = await spawn('tsc', args, {
+          shell: true,
+          windowsHide: true,
+          serialization: 'json'
+        });
+        resolve()
+      })
+    }
+    if (optionsProject.lowCpuUsage) {
+      return new Promise(async resolve => {
+        const terminal = await spawn('tsc', args, {
+          shell: true,
+          windowsHide: true,
+          serialization: 'json'
+        });
+
+        terminal.stdout.on('close', () => {
+          resolve(null)
+        })
+      })
+    }
+
+  } catch (err) {
+    throw err
+  }
 }
 
+
+const fixDir = (str = '') => {
+  if (str.startsWith('./')) {
+    str = './' + str
+  }
+  return str
+}
 
 const checkProject = (obj) => {
   if (obj.name == null) {
@@ -53,11 +108,38 @@ const checkProject = (obj) => {
     throw Error("TSC/JS - [0x001] It is necessary to inform the project's tsconfig.json settings. [target]")
   }
 
-  if (typeof obj.name !== 'string') {
+  if (typeof obj.name != 'string') {
     throw Error('TSC/JS - "-" The name must be a string input! [target]')
   }
-  if (typeof obj.projectDir !== 'string') {
+  if (typeof obj.projectDir != 'string') {
     throw Error('TSC/JS - "-" The projectDir must be a string input! [target]')
+  }
+  if (typeof obj.watchMode != 'boolean') {
+    obj.watchMode = false
+  }
+  return true
+}
+
+
+const checkProjectSettings = (obj) => {
+  if (obj.command == null) {
+    throw Error('TSC/JS - [0x0] You must have a command. Please check the field target! [target]')
+  }
+  if (obj.command == undefined) {
+    throw Error('TSC/JS - [0x000] You must have a command. Please check the field target! [target]')
+  }
+  if (typeof obj.hotReload != 'boolean') {
+    obj.hotReload = false
+  }
+  if (typeof obj.lowCpuUsage != 'boolean') {
+    obj.lowCpuUsage = false
+  }
+  if (typeof obj.targets != 'object') {
+    obj.targets = []
+  } else {
+    if (!Array.isArray(obj.targets)) {
+      obj.targets = []
+    }
   }
   return true
 }
@@ -70,23 +152,52 @@ module.exports = class ProjectWrapper extends EventEmitter {
   }
 
 
-  async start(dir, projects) {
+  async start(projects, options = {
+    'hotReload': true,
+    'command': "node .",
+    'lowCpuUsage': true,
+    'targets': []
+  }) {
     if (Array.isArray(projects)) {
       this.emit('startingProject', projects)
+      const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+      // Only projects that don't use watchMode
       for (const project of projects) {
-        if (checkProject(project)) {
-          const terminal = openTerminal(path.resolve('') + dir)
-          const eventTerminal = EventEmitter
-          this.emit('startingProject', terminal, this)
-          this.projects.set(project.name, {
-            terminal: terminal,
-            events: eventTerminal
-          })
+        if (project.watchMode == false) {
+          if (checkProject(project)) {
+            const terminal = await openTerminal(project.projectDir, project, options)
+            const eventTerminal = EventEmitter
+            this.emit('startingProject', terminal, this)
+            this.projects.set(project.name, {
+              terminal: terminal,
+              events: eventTerminal
+            })
+            await wait(3 * 1000)
+
+          } else {
+            throw Error('There was a problem with the project!')
+          }
+        }
+      }
+
+      // Only projects that use watchMode
+      for (const project of projects) {
+        if (project.watchMode == true) {
+          if (checkProject(project)) {
+
+            const terminal = openTerminal(project.projectDir, project, options)
+            const eventTerminal = EventEmitter
+            this.emit('startingProject', terminal, this)
+            this.projects.set(project.name, {
+              terminal: terminal,
+              events: eventTerminal
+            })
 
 
-          this.on('')
-        } else {
-          throw Error('There was a problem with the project!')
+          } else {
+            throw Error('There was a problem with the project!')
+          }
         }
       }
     } else {
@@ -95,14 +206,29 @@ module.exports = class ProjectWrapper extends EventEmitter {
     return this
   }
 
-  static async initialize(options = {
-    mode: 'hotreload'
-  }) {
+  static async initializeDefault() {
+    let targets = {}
+    let options = {
+      'hotReload': true,
+      'command': "node .",
+      'lowCpuUsage': true,
+      'targets': []
+    }
     const searchProjectSettings = await openDir(path.resolve(''))
     for (const file of searchProjectSettings) {
       if (file.endsWith('.json')) {
         if (file === 'projects.json') {
-          if (verify(path.resolve('') + file)) {
+          if (verify(path.resolve('projects.json'))) {
+            try {
+              const json = JSON.parse(await openFileAndRead(path.resolve('projects.json')))
+              checkProjectSettings(json)
+
+              targets = json.targets
+              options = json
+            } catch (err) {
+              console.error(err)
+              throw err
+            }
 
             break
           } else {
@@ -111,6 +237,6 @@ module.exports = class ProjectWrapper extends EventEmitter {
         }
       }
     }
-    return new ProjectWrapper().start()
+    return new ProjectWrapper().start(targets, options)
   }
 }
