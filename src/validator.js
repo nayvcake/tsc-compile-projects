b3c70f85582598fda515a863a1767a208941dc8b
nayvcake <43297329+nayvcake@users.noreply.github.com>
2022-06-26 03:59:07 -0300

    add(validator): Adding a data validator.
    Co-Authored-By: Nayv <43297329+nayvcake@users.noreply.github.com>

--- src/validator.js
+++ src/validator.js
@@ -1,11 +1,32 @@
-const validatorError = function () {
-  
-  return 
+/**
+ * @description Validating if the log is an error.
+ * @param {*} msg 
+ * @returns 
+ */
+const validatorError = function (msg = '') {
+  return msg.match(/error TS[0-9]+|error - TS[0-9]+/g).length != 0
+}
+/**
+ * @description Validating that it is a log that is not an error.
+ * @param {*} msg 
+ * @returns 
+ */
+const validatorAnyLog = function (msg = '') {
+  return msg.match(/[a-z] - TS[0-9]+|[a-z] TS[0-9]+/g).length != 0
+}
+/**
+ * @description Validating that the log is an error counter at the end of the build.
+ * @param {*} msg 
+ * @returns 
+ */
+const validatorCountOfError = function (msg = '') {
+  return msg.match(/[a-z] - TS[0-9]+|[a-z] TS[0-9]+/g).length != 0
 }
 
 
 
 module.exports.Validator = {
   validatorError,
-
+  validatorAnyLog,
+  validatorCountOfError
 }
