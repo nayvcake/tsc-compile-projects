7b5dd9301b7c9c9d943742128be1d858911e8c7e
Nayv <43297329+nayvcake@users.noreply.github.com>
2022-07-01 14:07:30 -0300

    Update index.js

--- example/index.js
+++ example/index.js
@@ -5,7 +5,7 @@ const ProjectWrapper = require('tsc-compile-projects')
 // Initialize the code.
 //
 //
-ProjectWrapper.initializeDefault({
+const build = ProjectWrapper.initializeDefault({
   // Hot loading can force application to restart as soon as it receives typescript events from final compilation and finally be restarted.
   //
   // You can disable and track development with automatic compilation.
@@ -13,7 +13,7 @@ ProjectWrapper.initializeDefault({
   
   // You can return just a string or in array to create more terminal.
   'command': [
-    ['node .']
+    ['node .', { /**   Shell Settings.   */}]
   ],
   
   // This setting is to compile the files calmly. As soon as another terminal finishes compiling the project wait a few seconds and then start another one. 
@@ -33,4 +33,10 @@ ProjectWrapper.initializeDefault({
       // Soon!
       'args': []
     }]
+})
+
+
+build.then((wrapper) => {
+  console.log(wrapper)
+  wrapper.on('logging', (data) => console.log(data))
 })
